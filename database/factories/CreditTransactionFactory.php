61472763d9a93ca83ee59e16ff24f949304ff324
Ivan Moroz <menvil.menvil@gmail.com>
2026-06-02 14:04:13 +0300

    CONV-199: Create credit transaction model and migration

---
+++ database/factories/CreditTransactionFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use App\Models\CreditTransaction;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<CreditTransaction>
+ */
+class CreditTransactionFactory extends Factory
+{
+    protected $model = CreditTransaction::class;
+
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'amount' => 10,
+            'balance_after' => 10,
+            'type' => 'grant',
+            'reason' => 'test',
+            'metadata_json' => null,
+        ];
+    }
+}
